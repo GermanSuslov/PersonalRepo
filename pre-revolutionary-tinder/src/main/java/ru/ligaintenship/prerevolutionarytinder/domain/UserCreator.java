d852dea01106c84103dd035e0ee329278b711ddf
GermanSuslov <germangame94@gmail.com>
2022-10-31 19:59:13 +0300

    Удалил ненужные строки

--- pre-revolutionary-tinder/src/main/java/ru/ligaintenship/prerevolutionarytinder/domain/UserCreator.java
+++ pre-revolutionary-tinder/src/main/java/ru/ligaintenship/prerevolutionarytinder/domain/UserCreator.java
@@ -13,11 +13,6 @@ public class UserCreator {
         String sql = "insert into tinder.tinder_users (sex, name, story, looking_for) values ('%s', '%s', '%s')"
                 .formatted(resource.getSex(), resource.getName(), resource.getStory(), resource.getLooking_for());
         int responseCode = provider.putData(sql);
-        User user = new User(resource.getId(),
-                resource.getName(),
-                resource.getSex(),
-                resource.getStory(),
-                resource.getLooking_for());
         return responseCode;
     }
 }
